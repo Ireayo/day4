20e393a56cc9548c181247df8758bed638e613c1
Apple <apple@Apples-MacBook-Pro-2.local>
2019-07-26 07:58:11 -0700

    finish ineration0

---
+++ actionsAndOutlets/actionsAndOutlets/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  actionsAndOutlets
+//
+//  Created by Apple on 7/25/19.
+//  Copyright © 2019 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textAppearshere: UILabel!
+   
+    
+    
+    
+    @IBOutlet weak var typeTexthere: UITextField!
+    
+    @IBAction func clickHere(_ sender: Any) {
+        if let newTitle = typeTexthere.text{
+          textAppearshere.text = newTitle
+        }
+    }
+    }
+    
+    
+    if case
+
+    
+
+
+
+    override func viewDidLoad() {
+        
+        
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hh/hh/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  hh
+//
+//  Created by Apple on 7/26/19.
+//  Copyright © 2019 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
